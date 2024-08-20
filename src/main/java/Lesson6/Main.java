32ef9b00d7970ef9dbdca09591e883dd5f98ff65
nasstulina <n.mishustina44@gmail.com>
2024-08-21 01:02:37 +0900

    Добавлено задание №2, без вывода в консоль

--- src/main/java/Lesson6/Main.java
+++ src/main/java/Lesson6/Main.java
@@ -14,12 +14,11 @@ public class Main {
         System.out.println("Задание №2");
         Person[] persArray = new Person[5];
         persArray[0] = new Person("Ivanov Ivan", "Teacher","ivivan@gmail.com", "89643254125", 45000, 34 );
-        System.out.println(Arrays.deepToString(persArray));
         persArray[1] = new Person("Ivannov Egor", "Doctor", "innegor@mail.ru", "89843453361", 25000, 23);
         persArray[2] = new Person("Egorov Ivan", "Engineer", "egorov@bk.ru", "89359862435", 35000, 26);
         persArray[3] = new Person("Petrov Igor", "Mechanic", "petrov23@gmail.com", "83457682539", 40000, 42);
         persArray[4] = new Person("Sidorov Petr", "Engineer", "sidpetr1995@gmail.com", "89321253684", 45000, 48);
-
+        System.out.println(Arrays.deepToString(persArray));
 
         System.out.println("\nЗадание №3. Создание класса \"Park\" c вложенным классом.");
         Park.infoAttraction park1 = new Park("Happines", 15,
@@ -48,16 +47,6 @@ public class Main {
                 this.salaryPerson = salaryPerson;
                 this.agePerson = agePerson;
             }
-            /*public void infoPerson(){
-                Person[] persArray = new Person[5];
-                persArray[0] = new Person("Ivanov Ivan", "Teacher","ivivan@gmail.com", "89643254125", 45000, 34 );
-                persArray[1] = new Person("Ivannov Egor", "Doctor", "innegor@mail.ru", "89843453361", 25000, 23);
-                persArray[2] = new Person("Egorov Ivan", "Engineer", "egorov@bk.ru", "89359862435", 35000, 26);
-                persArray[3] = new Person("Petrov Igor", "Mechanic", "petrov23@gmail.com", "83457682539", 40000, 42);
-                persArray[4] = new Person("Sidorov Petr", "Engineer", "sidpetr1995@gmail.com", "89321253684", 45000, 48);
-                System.out.println(Arrays.deepToString(persArray));
-            }*/
         }
-
 }
 
