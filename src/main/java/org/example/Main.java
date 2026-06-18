cd48fc7af28e38be8885d69b640dfa6aaf777f25
nasstulina <n.mishustina44@gmail.com>
2026-06-18 15:57:31 +0700

    Добавлена обработка исключений для двумерного массива и обработка исключения ArrayIndexOutOfBoundsException

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,52 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+    static int sumArray(String[][] arr) throws MyArraySizeException, MyArrayDataException {
+
+        if (arr.length != 4) {
+            throw new MyArraySizeException("Массив должен иметь длину 4");
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].length != 4) {
+                throw new MyArraySizeException("Строка " + i + " должна иметь ровно 4 элемента");
+            }
+        }
+
+        int a = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                try {
+                    a += Integer.parseInt(arr[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException("Неверный формат данных в ячейке [" + i + "][" + j + "]");
+                }
+            }
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        String arr[][] = {{"1", "2", "3", "4"},
+                {"1", "2", "3", "4"},
+                {"1", "2", "3", "4"},
+                {"1", "2", "3", "4"}};
+
+        try {
+            System.out.println("Сумма значений всех ячеек: " + sumArray(arr));
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            e.printStackTrace();
+        }
+
+
+        int array[] = {1, 2, 3, 4};
+
+        try {
+            System.out.println(array[10]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            e.printStackTrace();
         }
     }
 }
